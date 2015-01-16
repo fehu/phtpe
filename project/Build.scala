d0c0a9707441df150b4c00250400d0c359418605
fehu <lord.fehu@gmail.com>
2015-01-16 13:56:24 -0600

    travis: only 2.11.5;

--- project/Build.scala
+++ project/Build.scala
@@ -20,7 +20,7 @@ object  Build extends sbt.Build {
 
   lazy val testSettings = TestReportsCopy.settings ++ Seq(
     libraryDependencies += "org.specs2" %% "specs2" % "2.4.15" % "test",
-    resolvers ++= Seq("snapshots", "releases").map(Resolver.sonatypeRepo),
+    resolvers += "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases",
 
     TestReportsCopy.copyTestReportsDir <<= baseDirectory(base => Some(base / "test-reports")),
     TestReportsCopy.autoAddReportsToGit := true
